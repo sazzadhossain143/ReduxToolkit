8fd7df2e7e482e32552f41ac17e89b4a0d657d33
Sazzad Hossain <124303255+sazzadhossain143@users.noreply.github.com>
2025-11-13 12:39:44 +0600

    Delete book_CRUDapp directory

--- book_CRUDapp/src/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import { bookReducer } from './features/bookslice.js'
-
-export const store = configureStore({
-  reducer: {
-    // Add your reducers here
-    booksR: bookReducer,
-  },
-})
